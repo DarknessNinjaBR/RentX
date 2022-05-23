d1d491cc7f1df6e202ea57f94ac07c4dfa665f9b
Pedro Gomes Antunes <darkenessninjabr@hotmail.com>
2022-05-23 20:19:59 -0300

    refactoring and organizing files

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from "tsyringe";
 import { AppError } from "@shared/errors/AppError";
 
-import { ICategoriesRepository } from "@modules/cars/infra/typeorm/repositories/ICategoriesRepository";
+import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
 
 interface IRequest {
     name: string;
--- src/shared/infra/typeorm/migrations/1650750062199-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1650750062199-CreateUsers.ts
@@ -9,7 +9,8 @@ export class CreateUsers1650750062199 implements MigrationInterface {
                 columns: [
                     {
                         name: "id",
-                        type: "uuid"
+                        type: "uuid",
+                        isPrimary: true,
                     },
                     {
                         name: "name",
